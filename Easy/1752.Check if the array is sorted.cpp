83d5eb94ba45dd59c6ae326fc289e3effeb85917
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2025-11-23 07:45:06 +0530

    Add solution to check if array is sorted
    Implement a solution to check if an array is sorted with at most one rotation.

---
+++ Easy/1752.Check if the array is sorted.cpp
@@ -0,0 +1,18 @@
+//Time complexity: O(n)
+//Space complexity: O(1)
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        int rotations=0;
+        for(int i=1;i<n;++i){
+            if(nums[i]<nums[i-1]){
+                ++rotations;
+            }
+        }
+        if(nums[0]<nums[n-1]){
+            ++rotations;
+        }
+        return rotations<=1;
+    }
+};
