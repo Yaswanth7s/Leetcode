eabcdd505ef7ab97190007572c336c9762076eed
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2025-12-24 08:05:15 +0530

    Add rotate function for array rotation
    Implement array rotation by k places in C++.

---
+++ Medium/189.Rotate Array.cpp
@@ -0,0 +1,12 @@
+                                      //Rotation of array by k places
+//Time complexity: O(n)
+//Space complexity: O(1)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
