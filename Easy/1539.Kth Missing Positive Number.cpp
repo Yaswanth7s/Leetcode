82802a72c8e9a388ce345999517186ffa53ddfe5
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-01-04 10:31:35 +0530

    Add solution for Kth Missing Positive Number
    Implement binary search to find the Kth missing positive number.

---
+++ Easy/1539.Kth Missing Positive Number.cpp
@@ -0,0 +1,21 @@
+                                  //Finding the Missing Positive Number at Kth Position
+//Time complexity: O(log n)
+//Space complexity: O(1)
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low=0;
+        int high=arr.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high+1+k;
+    }
+};
