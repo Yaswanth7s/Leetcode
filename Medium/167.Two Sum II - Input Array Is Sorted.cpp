7585d6d0a4f59b3ae1e32e744f7c8cae0a982a60
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-01-08 21:25:31 +0530

    Fix comment formatting in Two Sum II solution

---
+++ Medium/167.Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,23 @@
+                                //Finding Two Sum (Type-II) When Input Array Is Sorted
+//Time complexity: O(n)
+//Space complexity: O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left=0;
+        int right=numbers.size()-1;
+        while(left<right){
+            int sum=numbers[left]+numbers[right];
+            if(sum==target){
+                return {left+1,right+1};
+            }
+            else if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return {};
+    }
+};
