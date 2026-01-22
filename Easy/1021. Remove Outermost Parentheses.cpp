266ba360d706aa05a3b3266053fecf3b8d747083
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-01-22 21:36:31 +0530

    Add solution for removing outermost parentheses
    Implement function to remove outermost parentheses from a string.

---
+++ Easy/1021. Remove Outermost Parentheses.cpp
@@ -0,0 +1,25 @@
+                                      //Removing the Outermost Parenthesis
+//Time complexity: O(n)
+//Space complexity: O(n)
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result="";
+        int level=0;
+        for(char ch:s){
+            if(ch=='('){
+                if(level>0){
+                    result+=ch;
+                }
+                level++;
+            }
+            else{
+                level--;
+                if(level>0){
+                    result+=ch;
+                }
+            }
+        }
+        return result;
+    }
+};
