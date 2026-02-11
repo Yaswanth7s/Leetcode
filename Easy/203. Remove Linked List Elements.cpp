d0b3d852ada1dbe55fb641a98916ace21b7256a1
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-02-11 23:08:44 +0530

    Add removeElements function for linked list
    Implement a function to remove elements with a specific value from a linked list.

---
+++ Easy/203. Remove Linked List Elements.cpp
@@ -0,0 +1,44 @@
+                                    //Deletion By Elements(Values)[with Duplicates]
+//Time complexity: O(n)
+//Sapce complexity: O(1)
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==nullptr){
+            return nullptr;
+        }
+        while(head!=nullptr && head->val==val){
+            ListNode* temp=head;
+            head=head->next;
+            delete(temp);
+        }
+        ListNode* temp=head;
+        ListNode* prev=nullptr;
+        while(temp!=nullptr){
+            if(temp->val==val){
+                prev->next=temp->next;
+                delete(temp);
+                temp=prev->next;
+            }
+            else{
+                prev=temp;
+                temp=temp->next;
+            }
+        }
+        return head;
+    }
+};
