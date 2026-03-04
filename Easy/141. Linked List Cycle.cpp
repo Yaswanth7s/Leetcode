73ad5353450625d702e8983c1360e92c65b59baf
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-03-04 10:05:15 +0530

    Detection Of Linked List Cycle.cpp

---
+++ Easy/141. Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+                                    //Detecting a Loop in a Linked List
+//Time complexity: O(N)
+//Space complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
