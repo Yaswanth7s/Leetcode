b053b35578fd6533c3d36effa770796c018aea94
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-03-12 00:17:22 +0530

    Intersection of Two Linked Lists.cpp

---
+++ Easy/160. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,28 @@
+                          //Finding the Intersection Node Of Two Linked Lists
+//Time complexity: O(N + M) where N = Nodes in List1
+                                //M=Nodes in List2
+//Space complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA==nullptr || headB==nullptr) return nullptr;
+        ListNode* t1=headA;
+        ListNode* t2=headB;
+        while(t1!=t2){
+            if(t1==nullptr) t1=headB;
+            else t1=t1->next;
+            if(t2==nullptr) t2=headA;
+            else t2=t2->next;
+        } 
+        return t1;
+    }
+};
