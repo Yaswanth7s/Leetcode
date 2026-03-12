bb9bce5bec67a6646d81917bf9c2c2df9c860448
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-03-12 11:14:34 +0530

    Create 2235. Add Two Integers.cpp

---
+++ Easy/2235. Add Two Integers.cpp
@@ -0,0 +1,11 @@
+                                    //Adding Two Integers
+                        
+//Time complexity: O(1)
+//Space complexity: O(1)
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        int sum=num1+num2;
+        return sum;
+    }
+};
