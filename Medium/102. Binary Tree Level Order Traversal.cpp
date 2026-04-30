123e06def518a3bff9f8d9818eb5ac8c0d963337
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-04-30 16:48:56 +0530

    Add binary tree level order traversal solution
    Implement level order traversal for a binary tree.

---
+++ Medium/102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,45 @@
+                                      //Level Order Traversal
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelorder(TreeNode* root,vector<vector<int>>& result){
+        if(root==nullptr) return result;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++){
+                TreeNode* temp=q.front();
+                q.pop();
+                level.push_back(temp->val);
+                if(temp->left!=nullptr){
+                    q.push(temp->left);
+                }
+                if(temp->right!=nullptr){
+                    q.push(temp->right);
+                }
+            }
+            result.push_back(level);
+        }
+        return result;
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        levelorder(root,result);
+        return result;
+    }
+};
