376340d086fb6585dbfca38fbaec4bf8bf9461ee
Yaswanth Reddy Somagattu <149325467+Yaswanth7s@users.noreply.github.com>
2026-05-13 22:04:51 +0530

    Add vertical order traversal for binary tree
    Implement vertical order traversal of a binary tree using BFS and a map to store nodes by their coordinates.

---
+++ Hard/987. Vertical Order Traversal of a Binary Tree.cpp
@@ -0,0 +1,47 @@
+                              //Vertical Order Traversal Of a Binary Tree
+//Time Complexity: O(n log n)
+//Space Complexity: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==nullptr) return ans;
+        map<int,map<int,multiset<int>>> nodes;
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            auto p=q.front();
+            q.pop();
+            TreeNode* temp=p.first;
+            int x=p.second.first;
+            int y=p.second.second;
+            nodes[x][y].insert(temp->val);
+            if(temp->left){
+                q.push({temp->left,{x-1,y+1}});
+            }
+            if(temp->right){
+                q.push({temp->right,{x+1,y+1}});
+            }
+        }
+        for(auto a:nodes){
+            vector<int> col;
+            for(auto b: a.second){
+                col.insert(col.end(),b.second.begin(),b.second.end());
+            }
+            ans.push_back(col);
+        }
+        return ans;
+    }
+};
